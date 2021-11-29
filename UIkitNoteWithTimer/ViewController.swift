b3d61a3ee86f5708b95cd5d2d62adef5f6f822c3
ryuji <ryujispjp@gmail.com>
2021-11-29 21:36:58 +0900

    描けるようにはなった

--- UIkitNoteWithTimer/ViewController.swift
+++ UIkitNoteWithTimer/ViewController.swift
@@ -8,52 +8,18 @@
 import UIKit
 import PencilKit
 
-class ViewController: UIViewController, PKCanvasViewDelegate{
+class ViewController: UIViewController{
 
-    private var canvasView: PKCanvasView!
-    private var toolPicker: PKToolPicker = {
-        if #available(iOS 14.0, *) {
-            return PKToolPicker()
-        } else {
-            return PKToolPicker.shared(for: UIApplication.shared.windows.first!)!
-        }
-    }()
 
-    private let defaultTool = PKInkingTool(.pen, color: .black, width: 1)
-    // for double tap action on Apple Pencil
-    private var currentTool: PKTool?
-    private var previousTool: PKTool?
-
-    // if a note is exist、a CollectionView set below properties
-    var drawing: PKDrawing?
-    var indexAtCollectionView: Int?
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        settingCanvas()
-    }
-    private func settingCanvas() {
-        canvasView = PKCanvasView(frame: view.frame)
-        canvasView.delegate = self
-        if let drawing = drawing {
-            canvasView.drawing = drawing
-        }
-        view.addSubview(canvasView)
-    }
-}
+        // Do any additional setup after loading the view.
 
-// MARK: PKToolPickerObserver
-extension ViewController: PKToolPickerObserver {
-    private func addPalette() {
-        toolPicker.addObserver(canvasView)
-        toolPicker.addObserver(self)
-        canvasView.becomeFirstResponder()
-        toolPicker.selectedTool = defaultTool
-        currentTool = defaultTool
-    }
-
-    func toolPickerSelectedToolDidChange(_ toolPicker: PKToolPicker) {
-        previousTool = currentTool
-        currentTool = toolPicker.selectedTool
+        // Three Lines of Code ここに3行のコードを足す
+        let canvas = PKCanvasView(frame: view.frame)
+        view.addSubview(canvas)
+        canvas.drawingPolicy = .anyInput
+        canvas.tool = PKInkingTool(.pen, color: .black, width: 30)
     }
 }
